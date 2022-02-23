f9c714243e3b480fcaf157392cf36e2a02c99cfa
Patrick Gannon <gannon.patrick01@gmail.com>
2022-02-23 00:05:48 -0500

    Final commit

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		// Ask how many new users we want to add
+		System.out.println("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		// Create n number of new students
+		for (int n = 0; n < numOfStudents; n++)
+		{
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		
+		for (int n = 0; n < numOfStudents; n++)
+		{
+			System.out.println(students[n].toString());
+		}
 	}
 
 }
